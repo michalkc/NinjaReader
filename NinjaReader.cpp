e4c1b225cf1dc17ce79fa969df28166293bfc349
Michał Kocon <nobooya@gmail.com>
2017-04-22 03:29:40 +0200

    Version 2

--- NinjaReader.cpp
+++ NinjaReader.cpp
@@ -1,3 +1,9 @@
+/*PROJEKT OINS*/
+
+/*STEGANOGRAFIA*/
+
+//Michal Kocon
+//Mateusz Chomiczewski
 
 #include <winsock2.h>
 #include <windows.h>
@@ -13,8 +19,7 @@
 void getHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header, UINT payload_len);
 static DWORD passthru(LPVOID arg);
 
-int __cdecl main(int argc, char **argv)
-{
+int __cdecl main(int argc, char **argv){
 	int num_threads = 1;
 	HANDLE handle, thread;
 
@@ -26,19 +31,16 @@ int __cdecl main(int argc, char **argv)
 									//	"tcp.PayloadLength > 0",    // TCP data packets only
 		WINDIVERT_LAYER_NETWORK, 0, WINDIVERT_FLAG_SNIFF //dont drop packets, just sniff them
 	);
-	if (handle == INVALID_HANDLE_VALUE)
-	{
+	if (handle == INVALID_HANDLE_VALUE){
 		std::cerr << "error: failed to open the WinDivert device: " << GetLastError() << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
 	// Start the threads
-	for (int i = 1; i < num_threads; i++)
-	{
+	for (int i = 1; i < num_threads; i++){
 		thread = CreateThread(NULL, 1, (LPTHREAD_START_ROUTINE)passthru,
 			(LPVOID)handle, 0, NULL);
-		if (thread == NULL)
-		{
+		if (thread == NULL){
 			std::cerr << "error: failed to start a thread" << std::endl;
 			exit(EXIT_FAILURE);
 		}
@@ -50,8 +52,7 @@ int __cdecl main(int argc, char **argv)
 	return 0;
 }
 
-static DWORD passthru(LPVOID arg)
-{
+static DWORD passthru(LPVOID arg){
 	unsigned char packet[MAXBUF];
 	UINT packet_len, payload_len;
 	WINDIVERT_ADDRESS addr;
@@ -62,11 +63,9 @@ static DWORD passthru(LPVOID arg)
 	bool addr_initialized = false;
 	UINT32 src_addr;
 	// Main loop:
-	while (true)
-	{
+	while (true){
 		// Read a matching packet.
-		if (!WinDivertRecv(handle, packet, sizeof(packet), &addr, &packet_len))
-		{
+		if (!WinDivertRecv(handle, packet, sizeof(packet), &addr, &packet_len)){
 			std::cerr << "Message read error: " << GetLastError() << std::endl;
 			continue;
 		}
@@ -85,11 +84,11 @@ void getHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header, 
 	static std::ofstream message;
 	static unsigned char id_last = 0;
 
-	if (bit_num == 8) {
+	if (bit_num == 8) { //open file on first use
 		if (!message.is_open()) {
 			std::string filename;
 			int file_num = 0;
-			while(true)  {
+			while(true)  { //check if file exists
 				filename = "hidden_message" + std::to_string(file_num) + ".txt";
 				if (std::ifstream(filename))
 					++file_num;
@@ -101,7 +100,7 @@ void getHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header, 
 			filename = "hidden_message" + std::to_string(file_num) + ".txt";
 			message.open(filename, std::ios::out | std::ios::binary);
 		}
-		else {
+		else { // if 8 bits were read, write whole byte to file and stdin
 			message << byte_to_write;
 			//message.flush();
 			std::cout <<byte_to_write;
@@ -110,9 +109,9 @@ void getHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header, 
 		}
 	}
 	unsigned char* byte = reinterpret_cast<unsigned char*>(&(ip_header->Id));
-	if (payload_len > 0 && bit_num < 8) {
+	if (payload_len > 0 && bit_num < 8) { //hidden message is present only when the frame contains payload
 		unsigned char diff = byte[1] - id_last;
-		if(diff == 2)
+		if(diff == 2) //if id jumped by 2, bit is set to 1
 			byte_to_write += (0x01 << bit_num);
 		++bit_num;
 	}
